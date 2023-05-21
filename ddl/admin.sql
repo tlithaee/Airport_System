841894ef296d4d6c2606af23d0f7107718692b27
Lita <90490665+tlithaee@users.noreply.github.com>
2023-05-21 11:47:47 +0700

    Create admin.sql

---
+++ ddl/admin.sql
@@ -0,0 +1,11 @@
+CREATE TABLE admin (
+    Ad_ID INT NOT NULL PRIMARY KEY,
+    Ad_Nama VARCHAR(60) NOT NULL,
+    Ad_No_Telp CHAR(15),
+    Ad_Jenis_Kelamin CHAR(1),
+    Ad_Email VARCHAR(30),
+    Ad_Alamat VARCHAR(100),
+    Ad_Tanggal_Lahir DATE,
+    Ak_ID INT NOT NULL,
+    FOREIGN KEY (Ak_ID) REFERENCES akses(Ak_ID)
+);
