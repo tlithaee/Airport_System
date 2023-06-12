eaf578efbe05bc0ec53cfb45d2f9ad7d496157ec
VictorGstn <125529445+VictorGstn@users.noreply.github.com>
2023-06-13 00:32:54 +0700

    Update backend pembayaran.php

--- pembayaran.php
+++ pembayaran.php
@@ -1,3 +1,15 @@
+<?php
+    include("config.php");
+    session_start();
+    $reservasi = 3;
+    $rid = 2;
+    //$reservasi = $_SESSION['r_id'] ;
+    $sql = "SELECT * FROM reservasi r JOIN harga h ON (h.Pn_ID = r.Pn_ID) WHERE R_ID = '$rid' AND h.K_ID = 'E';"; //belum sesuai, kurang k_id di reservasi
+    $query = mysqli_query($db, $sql);
+    while($row = mysqli_fetch_array($query)){
+        $harga = $row['H_Harga'];
+    } 
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +19,11 @@
         .navbar-brand {
             display: flex;
             align-items: center;
+        }
+        .confirm-but{
+            color: white;
+            background-color: royalblue;
+            border: transparent;
         }
     </style>
 </head>
@@ -24,25 +41,26 @@
         </nav>
 
     <main class="container">
+        <form action="proses-pembayaran.php" method="POST">
         <div class="payment">
             <div class="space">
                 <div class="d-flex space">
                     <label class="p-2 fs-7 fw-bold">Total Harga :</label>
                         <div class="d-flex fs-7 fw-bold">
-                            <input type="text" readonly class="form-control-plaintext" id="harga" value="Rp500.000,00-">
+                            <input type="text" readonly class="form-control-plaintext" id="harga" value="<?php echo $harga; ?>">
                         </div>
                 </div>
 
                 <div class="space">
                     <h6 class="p-2">Name on Card :</h6>
                         <div class="input-group mb-3">
-                            <input type="text" class="form-control" aria-label="Username" aria-describedby="basic-addon1" placeholder="Name">
+                            <input type="text" class="form-control" aria-label="Username" aria-describedby="basic-addon1" placeholder="Name" id="cardname" name="cardname">
                         </div>
                 </div>
 
                 <label class="p-2 ">Card Number :</label>
                 <div class="input-group mb-3">
-                    <input type="text" class="form-control" data-mask="0000 0000 0000 0000" placeholder="Card Number" aria-label="Username" aria-describedby="basic-addon1">
+                    <input type="text" class="form-control" data-mask="0000 0000 0000 0000" placeholder="Card Number" aria-label="Username" aria-describedby="basic-addon1" id="cardnum" name="cardnum">
                 </div>
                        
             </div>
@@ -51,18 +69,18 @@
                 <div class="col">
                     <h6 class="p-2">Valid Until :</h6>
                         <div class="input-group mb-2">
-                                <input type="date" class="form-control" aria-label="Username" aria-describedby="basic-addon1">
+                                <input type="date" class="form-control" aria-label="Username" aria-describedby="basic-addon1" id="validdate" name="validdate">
                         </div>
                 </div>
                 <div class="col">
                     <h6 class="p-2">CVV :</h6>
                         <div class="input-group mb-3">
-                            <input type="type" class="form-control" aria-label="Username" data-mask="000" placeholder="000" aria-describedby="basic-addon1">
+                            <input type="type" class="form-control" aria-label="Username" data-mask="000" placeholder="000" aria-describedby="basic-addon1" id="cvv" name="cvv">
                         </div>
                 </div>
                
                 <div class="d-flex align-items-center flex-column mt-auto">
-                    <button type="button" class="btn btn-primary d-flex align-items-center flex-column" style="height: 50px; font-size: 16px;" data-bs-toggle="modal" data-bs-target="#passModal">
+                    <button onclick="submit()" type="button" class="btn btn-primary d-flex align-items-center flex-column" style="height: 50px; font-size: 16px;" data-bs-toggle="modal" data-bs-target="#passModal">
                     Bayar
                     </button>
                 </div>
@@ -83,16 +101,19 @@
                             <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
                                 <div class="modal-body d-flex align-items-center flex-column mt-auto">
                                     <h5 class="p-2 fw-bold booking">
-                                        Kode Booking : 08.01x31.12
+                                        Kode Booking : <?php echo $rid; ?>
                                         <input type="text" readonly class="form-control-plaintext" id="booking" value="">
                                     </h5>
                                 </div>
                             </form>
+                            <input class="confirm-but" style="height: 50px; font-size: 16px;"  type="submit" value="Konfirmasi" name="submit">
                         </div>
                     </div>
                 </div>
+            </div>
+        </div>
+        </form>
     </main>
-    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 
 </body>
