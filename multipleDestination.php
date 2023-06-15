be1a98911d3a61e4248ff1ab89895ea6a95f0b00
Gabrielle Immanuel Osvaldo Kurniawan <108170210+Osvaldo-Kurniawan@users.noreply.github.com>
2023-06-15 20:27:19 +0700

    Update multipleDestination.php

--- multipleDestination.php
+++ multipleDestination.php
@@ -3,7 +3,7 @@
 <html>
 <head>
 	<title>Admin</title>
-	<link rel="stylesheet" href="asset/style.css">
+	<link rel="stylesheet" href="assets/css/admin.css">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 <body>
